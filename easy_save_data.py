8dfcbb2181ac360bc1122c4c79c9774136b0cae2
shoemaker <tonghanshuang.thu@gmail.com>
2021-04-06 16:12:25 +0800

    add src

---
+++ easy_save_data.py
@@ -0,0 +1,54 @@
+# @Project  :adaptive_topic_recommendation
+# @File     :load_data_util
+# @Date     :2021/1/7 14:37
+# @Author   :Hanshuang Tong
+# @Email    :tonghanshuang.thu@gmail.com
+# @Software :PyCharm
+import json
+import pickle
+
+import numpy as np
+
+from src.built_excel import BuildStyleTable
+
+
+def save_pkl(path, data):
+    pickle.dump(data, open(path, 'wb'))
+
+
+def save_csv(path, data):
+    data.to_csv(path, index=False, header=True, encoding='utf_8_sig')
+
+
+def save_excel(path, data, format=False):
+    if not format:
+        data.to_excel(path, index=False, header=True, encoding='utf_8_sig')
+    else:
+        a_d = {k: list(data[k]) for k in data.columns}
+        all_diagnosis_table = BuildStyleTable()
+        title_style = all_diagnosis_table.get_title_style()
+        mid_style = all_diagnosis_table.get_mid_style()
+        style_dict = {"title": title_style}
+        for k in a_d:
+            style_dict[k] = mid_style
+        all_diagnosis_table.build_sheet("Sheet1", a_d, style_dict, [])
+        all_diagnosis_table.workbook.save(path)
+
+
+def save_txt(path, data):
+    with open(path, "w") as f:
+        for i in data:
+            f.writelines(i)
+            f.writelines("\n")
+
+
+def save_json(path, data, visual_only=False):
+    if visual_only:
+        data = {k: {i: str(data[k][i]) for i in data[k]} for k in data}
+    json_str = json.dumps(data, indent=4, ensure_ascii=False)
+    with open(path, 'w', encoding='utf-8') as json_file:
+        json_file.write(json_str)
+
+
+def save_npy(path, data):
+    np.save(path, data)
---
+++ setup.py
@@ -0,0 +1,44 @@
+# @Project  :easyload
+# @File     :setup
+# @Date     :2021/4/6 14:28
+# @Author   :tonghanshuang
+# @Email    :tonghanshuang.thu@gmail.com
+# @Software :PyCharm
+
+from setuptools import setup
+
+setup(
+    # 以下为必需参数
+    name='easyload',  # 模块名
+    version='1.0.0',  # 当前版本
+    description='A Python library for loading multiple types of data',  # 简短描述
+    py_modules=["my_module"],  # 单文件模块写法
+    # ckages=find_packages(exclude=['contrib', 'docs', 'tests']),  # 多文件模块写法
+
+    # 以下均为可选参数
+    long_description="",  # 长描述
+    url='https://github.com/pypa/sampleproject',  # 主页链接
+    author='Shoemaker',  # 作者名
+    author_email='tonghanshuang.thu@gmail.com',  # 作者邮箱
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # 当前开发进度等级（测试版，正式版等）
+
+        'Intended Audience :: Developers',  # 模块适用人群
+        'Topic :: Software Development :: Build Tools',  # 给模块加话题标签
+
+        'License :: OSI Approved :: MIT License',  # 模块的license
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='easy load data',  # 模块的关键词，使用空格分割
+    install_requires=['xlwt','numpy','pandas'],  # 依赖模块
+
+    project_urls={  # 项目相关的额外链接
+        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+        'Source': 'https://github.com/pypa/sampleproject/',
+    },
+)
